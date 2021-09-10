2331f65bfac912c9151fbec0a2d3434e75394ace
sayitsamikoca0 <sayitsamikoca0@gmail.com>
2021-09-10 23:42:05 +0300

    Assignments 3 days

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public  void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : "+ musteri.MusteriAdi);
+        }
+
+        public void Listing(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi: "+ musteri.MusteriAdi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAdi = "Kazım";
+            musteri1.MusteriSoyadi = "Kar";
+            musteri1.MusteriBakiye = 1200;
+
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.MusteriId = 1;
+            musteri2.MusteriAdi = "Sami";
+            musteri2.MusteriSoyadi = "Koca";
+            musteri2.MusteriBakiye = 500;
+
+
+            Musteri musteri3 = new Musteri();
+
+            musteri3.MusteriId = 1;
+            musteri3.MusteriAdi = "Kevin";
+            musteri3.MusteriSoyadi = "Mittnick";
+            musteri3.MusteriBakiye = 150;
+
+            Musteri[] Musteriler = new Musteri[] {musteri1, musteri2, musteri3 };
+
+            MusteriManager manager = new MusteriManager();
+            manager.Add(musteri1);
+            Console.WriteLine("\n ---------------------------------------------------- \n");
+            Console.WriteLine("********* Müşteriler Listelendi ********** \n"); 
+            manager.Listing(musteri1);
+            manager.Listing(musteri2);
+            manager.Listing(musteri3);
+            Console.WriteLine("-------------------------------------------------------------");
+            manager.Delete(musteri2);
+
+        }
+    }
+}
